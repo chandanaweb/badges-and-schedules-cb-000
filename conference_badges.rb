0a397edce84495c6aa60916134bf9a8744793be3
chandana M <chandu11288@gmail.com>
2018-02-08 20:40:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,8 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  room_no =1
-  speakers.each do |speaker|
-    return "Hello, #{speaker}! You'll be assigned to room #{room_no}!"
-    room_no += 1
+  speakers.each_with_index { |speaker,index|
+    return "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    }  
   end
 end
