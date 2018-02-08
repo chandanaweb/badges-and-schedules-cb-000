03820090674db9550fc70b3103701a8f141f8bf0
chandana M <chandu11288@gmail.com>
2018-02-08 20:36:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ end
 def assign_rooms(speakers)
   room_no =1
   speakers.each do |speaker|
-    puts "Hello, #{speaker}! You'll be assigned to room #{room_no}!"
+    return "Hello, #{speaker}! You'll be assigned to room #{room_no}!"
     room_no += 1
   end
 end
