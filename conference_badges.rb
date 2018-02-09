35a3f3f85bf1d1a03bad77ee83c7c989ab038db3
chandana M <chandu11288@gmail.com>
2018-02-09 13:47:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,9 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-
+ batch_badge_creator(attendees).each do |attendee|
+   puts "#{attendee}"
+ end
   assign_rooms(attendees).each do |attendee|
     puts "#{attendee}"
   end
